fb5dd65b2dddb43480c9dbca7ac4531768d8f887
JoshTriangleTechDeveloper <joshwebb8137@gmail.com>
2020-07-23 11:29:09 -0400

    Code restructuring / added images

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const getDogImages = (numOfImages) => {
+  //take number input and loop 
   for (let i = 0; i < numOfImages; i++) {
     if (i < numOfImages) {
       console.log(i);
+      //call dog API
       fetch('https://dog.ceo/api/breeds/image/random')
         .then(response => response.json())
         .then(responseJson => 
@@ -15,24 +17,27 @@ const getDogImages = (numOfImages) => {
 
 const displayResults = (responseJson) => {
   console.log(responseJson);
-  //replace the existing image with the new one
-  $('.results-img').html("");
+  //add images
   $('.pictureDisplay').append(
-    `<img src="${responseJson.message}" class="results-img" width="250" height="200" style="margin: 10px; border-radius: 10px;">`
+    `<img src="${responseJson.message}" class="results-img" width="auto" height="225" style="margin: 10px; border-radius: 10px;">`
   );
   //display the results section
-  $('h2').removeClass('hidden');
+  $('h2, p').removeClass('hidden');
 }
 
 const watchForm = () => {
+  //on submit clear any current pictures and pass number value to
+  //getDogImages function
   $('form').submit(event => {
     event.preventDefault();
+    $('.pictureDisplay').html("");
     let numOfImages = $('#picQuantity').val();
     getDogImages(numOfImages);
   });
 }
 
 $(function() {
+  //on ready run watchForm function
   console.log('App loaded! Waiting for submit!');
   watchForm();
 });
